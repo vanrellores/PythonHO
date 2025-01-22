80b50c1ec0db337043c5d8ccd2fc22097f069cb6
Van Marx Rellores <vanrellores@gmail.com>
2025-01-23 03:58:54 +0800

    nested if

--- 1.py
+++ 1.py
@@ -1,18 +1,42 @@
 import random
 
 
-def get_choices():
-    player_choice = input("Enter choice: (rock, paper, scissors)")
-    computer_choice = ["rock", "paper", "scissors"]
+# def get_choices():
+#     player_choice = input("Enter choice: (rock, paper, scissors)")
+#     computer_choice = ["rock", "paper", "scissors"]
 
-    choyz = {
-        "computer": random.choice(computer_choice),
-        "player": player_choice
-    }
+#     choyz = {
+#         "computer": random.choice(computer_choice),
+#         "player": player_choice
+#     }
 
-    return choyz
-
-choice = get_choices()
-print(choice)
+#     return choyz
 
 
+def check_win(player, computer):
+    print(f"You chose {player}, " + f"Computer chose {computer}")
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock":
+        if computer == "scissors":
+            print("You win!")
+        else:
+            print("You lose.")
+    elif player == "scissors":
+        if computer == "paper":
+            print("You win!")
+        else:
+            print("You lose.")
+    else:
+        if computer == "rock":
+            print("You win!")
+        else:
+            print("You lose.")
+
+def get_choice():
+    P = input("Enter choice: (rock, paper or scissors)")
+    Com_choice = ["rock", "paper", "scissors"]
+    C = random.choice(Com_choice)
+    return P, C
+check_win(*get_choice())
+    
