42f2e9f6b54744019dfbd84c435e25ef84f43978
Van Marx Rellores <vanrellores@gmail.com>
2025-02-22 13:33:31 +0800

    Number Operations 02-22-2025

---
+++ Exercises/NumOps.py
@@ -0,0 +1,10 @@
+# from datetime import date
+# from requests import 
+
+import math
+
+print(math.sqrt(4))
+print(math.fabs(-5))
+print(math.floor(4.6))
+print(math.pow(2,2))
+print(math.trunc(7.999))
